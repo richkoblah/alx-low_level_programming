41492734ea3157ee427e5e64205387c4f4a8edff
richkoblah <rk.agbehadzi@outlook.com>
2022-10-14 13:54:04 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main-entry point for the program.
+ * Return:0 if no errors, return non zero if errors
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	printf("\n");
+
+	return (0);
+}
