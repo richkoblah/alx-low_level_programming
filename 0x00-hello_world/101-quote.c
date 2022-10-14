45df1ffdfceaed31df4fe1daba33d9192fd83434
richkoblah <rk.agbehadzi@outlook.com>
2022-10-13 18:18:56 -0700

    Task 8.  UNIX is basically a simple operating system

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <unistd.h>
 /**
    *main-program entry point.
-    *
-     *Return: 1 if successful
-      */
+   *
+   *Return: 1 if successful
+ */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-1\n ,59");
+	write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-1\n", 59);
 	return (1);
 }
