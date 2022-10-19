88b17d4710cab7fee926c480d92aa68dc9e4f04f
richkoblah <rk.agbehadzi@outlook.com>
2022-10-19 16:40:17 -0700

    9. Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,31 +3,39 @@
   * times_table - Prints the 9 times table
   * Return: no return
   */
-
 void times_table(void)
 {
-	int a, b, op;
+	int x, y, z, u, d;
 
-	for (a = 0; a <= 9; a++)
+	for (x = 0; x <= 9; x++)
 	{
-		_putchar(48);
-		for (b = 1; b <= 9; b++)
+		for (y = 0; y <= 9; y++)
 		{
-			op = a * b;
-			_putchar(44);
-			_putchar(32);
-			_putchar(32);
-			if (op <= 9)
+			z = x * y;
+
+			if (z > 9)
 			{
+				u = z % 10;
+				d = (z - u) / 10;
+
+				_putchar(44);
 				_putchar(32);
-				_putchar(op + 48);
+				_putchar(d + '0');
+				_putchar(u + '0');
 			}
 			else
 			{
-				_putchar((op / 10) + 48);
-				_putchar((op % 10) + 48);
+				if (y != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
+				_putchar(z + '0');
 			}
 		}
 		_putchar('\n');
 	}
 }
+				
+
