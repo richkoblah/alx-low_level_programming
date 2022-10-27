1bc6c4eb7a532069a5c8278d358c44406d9b08b7
richkoblah <rk.agbehadzi@outlook.com>
2022-10-27 11:34:25 -0700

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,12 +9,22 @@
  *         If str1 == str2, 0.
  *         If str1 > str2, the positive difference of the first unmatched 
  */
+
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 && *s2 && *s1 == *s2)
+	int i;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
-		s1++;
-		s2++;
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	}
+	return (0);
+}
+	
+
 	}
 
 	return (*s1 - *s2);
