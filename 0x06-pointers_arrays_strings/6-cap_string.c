b336f96ddc5c180a356de95714cb5913d261ca43
richkoblah <rk.agbehadzi@outlook.com>
2022-10-27 11:51:58 -0700

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,7 +6,7 @@
  *
  * Return: A pointer to the changed string.
  */
-char *cap_string(char *)
+char *cap_string(char *str)
 {
 	int index = 0;
 
@@ -14,7 +14,6 @@ char *cap_string(char *)
 	{
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
-
 		if (str[index - 1] == ' ' ||
 		    str[index - 1] == '\t' ||
 		    str[index - 1] == '\n' ||
