2ae4282c1770c94993a96de3d4d66dda8b61c3c0
richkoblah <rk.agbehadzi@outlook.com>
2022-10-27 09:31:33 -0700

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,13 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * File: main.h
- * Auth: Richard Agbehadzi
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x06-pointers_arrays_strings directory.
- */
-
 #include <stddef.h>
 
 int _putchar(char c);
@@ -23,6 +16,5 @@ char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
-
 
 #endif /* MAIN_H */
