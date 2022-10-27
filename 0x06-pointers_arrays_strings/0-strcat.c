a3e7a5d8837df84a9164bc1026675838f224a152
richkoblah <rk.agbehadzi@outlook.com>
2022-10-27 10:08:29 -0700

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,16 +4,18 @@
  *          null byte, to the end of the string pointed to by @dest.
  * @dest: A pointer to the string to be concatenated upon.
  * @src: The source string to be appended to @dest.
- *
  * Return: A pointer to the destination string @dest.
  */
-char *strcat(char *dest, const char *src)
+char *_strcat(char *dest, char *src)
 {
-	int i, j
+	int i, j;
 
-		i = 0, j = 0;
+	i = 0;
+	j = 0;
 
 	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
 		j++;
