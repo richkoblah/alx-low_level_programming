5af7ad41344d3972282b000b803398f4fb0c0818
richkoblah <rk.agbehadzi@outlook.com>
2022-12-02 02:54:18 -0800

    0x14. C - Bit manipulation

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -9,7 +9,7 @@
  *       written in the 0x13-bit_manipulation directory.
  */
 
-int _putchar(char c;
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
